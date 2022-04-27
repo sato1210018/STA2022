0506dbb7e74b069c4fc0aff8059d4bd72395321b
1210018 <cbhs1210018@gn.iwasaki.ac.jp>
2022-04-27 15:52:00 +0900

    see you later!

---
+++ python1/option1.py
@@ -0,0 +1,17 @@
+i = 30
+
+while i <= 60:
+	j = 2
+	isPrime = False
+
+	while j < i:
+		if i % j == 0:
+			break
+		j += 1
+	if J == i:
+		isPrime = True 
+	if isprime:
+		print("prime")
+	else:
+		print(i)
+	i += 1
